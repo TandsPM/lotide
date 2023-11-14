d8be5bcdf91b540ce52c8be5dfd6395f080d9353
Tandra Malm <tandra_malm@hotmail.com>
2023-11-13 20:24:03 -0700

    Add changes to file

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: [actual] === [expected]");
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   }
 
   if (actual !== expected) {
-    console.log("🟥🟥🟥 Assertion Failed: [actual] !== [expected]");
+    console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
