1b0d5adad6521e82dbb1fae6645deacd690d5e04
Tandra Malm <tandra_malm@hotmail.com>
2023-11-14 15:31:29 -0700

    Add changes to the tail.js file

--- tail.js
+++ tail.js
@@ -4,7 +4,7 @@ const tail = function(arr) {
 module.exports = tail;
 
 
-/*const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   }
@@ -13,6 +13,9 @@ module.exports = tail;
     console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-*/
 
 
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
