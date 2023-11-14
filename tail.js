641e9a0d3b09531a5c1e6f43863b6dbd481d6434
Tandra Malm <tandra_malm@hotmail.com>
2023-11-14 08:54:56 -0700

    Add file to Github

---
+++ tail.js
@@ -0,0 +1,18 @@
+const tail = function(arr) {
+  return arr.slice(1);
+};
+module.exports = tail;
+
+
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+*/
+
+
