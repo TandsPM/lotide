206b5605735876a1ea4bd267e820fdc7b939ddad
Tandra Malm <tandra_malm@hotmail.com>
2023-11-13 20:20:16 -0700

    add file to Github

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: [actual] === [expected]");
+  }
+
+  if (actual !== expected) {
+    console.log("🟥🟥🟥 Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(15, 12);
+assertEqual("Coding Project", "Coding Project");
