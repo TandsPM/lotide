10d24a29f13795d93a231f5838754fba42ef8081
Tandra Malm <tandra_malm@hotmail.com>
2023-11-13 20:35:48 -0700

    add file to Github

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function(item) {
+  if (item.length === 0) {
+    return undefined; //this is for an empty array
+  }
+  return item[0];
+} 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
