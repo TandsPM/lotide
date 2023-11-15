69878ee6637333b65cede1d6fe6fe72cfeace26b
Tandra Malm <tandra_malm@hotmail.com>
2023-11-14 20:40:05 -0700

    Add file to Github

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// Take two arrays and return true or false based on a perfect match ===
+const eqArrays = function(arr1, arr2) {
+  return arr1 === arr2;
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/*assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(15, 12);
+assertEqual("Coding Project", "Coding Project");
+*/
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
