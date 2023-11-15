91949f5114644760e253be528ed50264fcdf223b
Tandra Malm <tandra_malm@hotmail.com>
2023-11-14 21:52:43 -0700

    add changes to file eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -32,9 +32,9 @@ assertEqual("Coding Project", "Coding Project");
 */
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
