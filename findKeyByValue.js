5bfbfd7e241b6abdd72afe1b07d9c7961bf447ee
Tandra Malm <tandra_malm@hotmail.com>
2023-11-16 08:26:38 -0700

    add changes to file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -4,13 +4,15 @@
 
 
 const findKeyByValue = function(obj, value) {
-  // use Object.keys()
+  // use Object.keys() - get a list of all the categories (keys) in the list of TV shows (object)
+  // used mdn to look this up
   const keys = Object.keys(obj);
 
-    // iterate over the keys
+  // iterate over the keys - check 1 by 1
   for (const key of keys) {
-// check if value at current key = the value parameter
+  // check if value at current key = the value parameter
   if (obj[key] === value) {
+    // return the hey where we found the show
     return key;
     } else {
       // if no key with that given value - return undefined
