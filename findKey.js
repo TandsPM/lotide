90ed36e507df973f792acc07919f45b43afb2523
Tandra Malm <tandra_malm@hotmail.com>
2023-11-17 15:16:28 -0700

    add file to github

---
+++ findKey.js
@@ -0,0 +1,46 @@
+// scan the object and return the first key for which the callback returns a truthy value
+// If no key is found, then it should return undefined
+
+const findKey = function(obj, callback) {
+  for (const key in obj) {
+    console.log('key: ', key);
+    if (callback(obj[key])) {
+    console.log('key: ', key);
+  return(key);
+    }
+  }
+   return undefined;
+}
+
+const keyValue = function(obj, value) {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(findKey, "elBulli");
+assertEqual(findKey, "noma");
+
