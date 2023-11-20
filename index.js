dd3c5bbf9fe5b6612c2e13f282a7d3749fab5d17
Tandra Malm <tandra_malm@hotmail.com>
2023-11-20 15:56:52 -0700

    add changes

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArratsEqual: assertArraysEqual
+
 };
