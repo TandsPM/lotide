dab8e2f2b54dc459a79d7780315765c901e3db55
Tandra Malm <tandra_malm@hotmail.com>
2023-11-20 15:40:09 -0700

    update file

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,23 +1,8 @@
-const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
 
-
-describe("#assertEqual", () => {
-  it("return false if !== same item", () => {
-    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"), undefined);
-  });
-
-  it("return true if === same item", () => {
-    assert.strictEqual(assertEqual("Coding Project", "Coding Project"), undefined);
-  });
-
-  it("return false if !== same item", () => {
-    assert.strictEqual(assertEqual(15, 12), undefined);
-  });
-
-  it("return true if === same item", () => {
-    assert.strictEqual(assertEqual(1, 1), undefined);
-  });
-});
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(15, 12);
+assertEqual("Coding Project", "Coding Project");
 
