2cad569082a4883beed962afb8e70e669f60ee1e
Tandra Malm <tandra_malm@hotmail.com>
2023-11-20 14:59:31 -0700

    Add file to github

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,28 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+/*
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); 
 assertEqual(words.length, 3);
+*/
+
+
+describe("#tail", () => {
+  it("returns the tail end of element, only excluding the first element - for ['Yo Yo', 'Lighthouse', 'Labs'], return ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns the tail end of element, only excluding the first element - for [], return []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("returns the tail end of element, only excluding the first element - for ['lighthouse'], return []", () => {
+    assert.deepEqual(tail(['Lighthouse']), []);
+  });
+
+  it("returns the tail end of element, only excluding the first element - for ['Yo Yo', 'Lighthouse'], return ['Lighthouse']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse']), ['Lighthouse']);
+  });
+
+});
