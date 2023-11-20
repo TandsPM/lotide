8d3aa79026056a733425ae95459a9342501bb978
Tandra Malm <tandra_malm@hotmail.com>
2023-11-20 15:40:58 -0700

    update file

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,7 @@
-const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
-/*
+
+
+
 const array1 = [1, 2, 3];
 const array2 = [1, 2, 3];
 assertArraysEqual(array1, array2);
@@ -12,16 +13,4 @@ assertArraysEqual(arr1, arr2);
 const arr3= [1, 2, 3];
 const arr4 = [1, 2, 3, 4];
 assertArraysEqual(arr3, arr4);
-*/
 
-
-describe("#assertEqual", () => {
-  it("return true if === same array", () => {
-    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), undefined);
-  });
-
-  it("return false if === same array", () => {
-    assert.strictEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), undefined);
-  });
-
-});
