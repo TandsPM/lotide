aaec471afec6ccd56869d8f3099456796d830b42
Tandra Malm <tandra_malm@hotmail.com>
2023-11-20 10:22:51 -0700

    add and update files to github

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-    return false;
-    // AI suggest to replace the above code with
-    // return arr1.every((val, index) => val === arr2[index]);
-    }
-    }
-    return true;
-  }
-  
-
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays (actual, expected)) {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
@@ -21,3 +5,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
   }
 }
+
+module.export = assertArraysEqual
